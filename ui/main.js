3169d7059464228e96a32eb71509d16e715ca99a
vamsik11 <31630138+vamsik11@users.noreply.github.com>
2017-09-07 09:54:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,11 @@ var element =document.getElementById('main-text');
 element.innerHTML='New Value';
 //for moving the image in the webpage
 var img =document.getElementById('maddy');
-
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var internal=setInternal(moveRight,50);
 };
