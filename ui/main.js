5015bc9af8843e81c9572f32a8e635fbc21f1adf
vamsik11 <31630138+vamsik11@users.noreply.github.com>
2017-09-07 09:39:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
+//for changing text in the webpage
 var element =document.getElementById('main-text');
 element.innerHTML='New Value';
+//for moving the image in the webpage
+var element =document.getElementById('maddy');
+
+img.onclick=function(){
+    img.style.marginright='100px';
+};
